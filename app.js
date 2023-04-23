1636b952799142ed7f4ef964ed09c4a1731296bb
Grant Simon <grant.simon-2@selu.edu>
2023-04-22 21:31:54 -0500

    Am I done?

--- app.js
+++ app.js
@@ -1,111 +1,153 @@
-const MongoClient = require("mongodb").MongoClient
+const express = require("express")
+const bodyParser = require("body-parser")
+
+//Pre-Baked Code to Confirm Connection to MongoDB
+const { MongoClient, ServerApiVersion } = require("mongodb")
 const uri =
 	"mongodb+srv://admin:415project@415-project.qf5zcil.mongodb.net/?retryWrites=true&w=majority"
+
 const client = new MongoClient(uri, {
-	useNewUrlParser: true,
-	useUnifiedTopology: true,
+	serverApi: {
+		version: ServerApiVersion.v1,
+		strict: true,
+		deprecationErrors: true,
+	},
 })
-client.connect((err) => {
-	if (err) {
-		console.error(err)
-		return
+
+async function run() {
+	try {
+		await client.connect()
+		await client.db("admin").command({ ping: 1 })
+		console.log("Pinged your deployment. You successfully connected to MongoDB!")
+	} finally {
+		await client.close()
 	}
-	console.log("Connected to MongoDB")
+}
+run().catch(console.dir)
 
-	const db = client.db("my-database")
-    app.use(bodyParser.urlencoded({ extended: true }));
+//Start of Endpoints
+const app = express()
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+const db = client.db("415-Project")
 
-	app.get("/rest/list", function (req, res) {
-		db
-			.collection("tickets")
-			.find()
-			.toArray(function (err, docs) {
-				if (err) {
-					console.error(err)
-					res.status(500).send("Error retrieving tickets")
-					return
-				}
-				res.send(docs)
-			})
-	})
+const tickets = [
+	{
+		id: 1,
+		title: "Ticket 1",
+		description: "Description 1",
+	},
+	{
+		id: 2,
+		title: "Ticket 2",
+		description: "Description 2",
+	},
+	{
+		id: 3,
+		title: "Ticket 3",
+		description: "Description 3",
+	},
+]
 
-	app.get("/rest/ticket/:id", function (req, res) {
-		const id = req.params.id
-		db
-			.collection("tickets")
-			.findOne({ _id: new mongodb.ObjectId(id) }, function (err, doc) {
-				if (err) {
-					console.error(err)
-					res.status(500).send("Error retrieving ticket")
-					return
-				}
-				if (!doc) {
-					res.status(404).send("Ticket not found")
-					return
-				}
-				res.send(doc)
-			})
-	})
-
-	app.post("/rest/ticket", function (req, res) {
-		const ticket = req.body
-		db.collection("tickets").insertOne(ticket, function (err, result) {
+app.get("/rest/list", function (req, res) {
+	db
+		.collection("tickets")
+		.find()
+		.toArray(function (err, docs) {
 			if (err) {
 				console.error(err)
-				res.status(500).send("Error creating ticket")
+				res.status(500).send("Error retrieving tickets")
 				return
 			}
-			res.send(result.ops[0])
+			res.send(docs)
 		})
-	})
+})
 
-	app.delete("/rest/ticket/:id", function (req, res) {
-		const id = req.params.id
-		db
-			.collection("tickets")
-			.deleteOne({ _id: new mongodb.ObjectId(id) }, function (err, result) {
+app.get("/rest/ticket/:id", function (req, res) {
+	const id = req.params.id
+	db
+		.collection("tickets")
+		.findOne({ _id: new mongodb.ObjectId(id) }, function (err, doc) {
+			if (err) {
+				console.error(err)
+				res.status(500).send("Error retrieving ticket")
+				return
+			}
+			if (!doc) {
+				res.status(404).send("Ticket not found")
+				return
+			}
+			res.send(doc)
+		})
+})
+
+app.post("/rest/ticket", function (req, res) {
+	const ticket = req.body
+	db.collection("tickets").insertOne(ticket, function (err, result) {
+		if (err) {
+			console.error(err)
+			res.status(500).send("Error creating ticket")
+			return
+		}
+		res.send(result.ops[0])
+	})
+})
+
+app.delete("/rest/ticket/:id", function (req, res) {
+	const id = req.params.id
+	db
+		.collection("tickets")
+		.deleteOne({ _id: new mongodb.ObjectId(id) }, function (err, result) {
+			if (err) {
+				console.error(err)
+				res.status(500).send("Error deleting ticket")
+				return
+			}
+			if (result.deletedCount === 0) {
+				res.status(404).send("Ticket not found")
+				return
+			}
+			res.sendStatus(204)
+		})
+})
+
+app.put("/rest/ticket/:id", function (req, res) {
+	const id = req.params.id
+	const updates = req.body
+	db
+		.collection("tickets")
+		.updateOne(
+			{ _id: new mongodb.ObjectId(id) },
+			{ $set: updates },
+			function (err, result) {
 				if (err) {
 					console.error(err)
-					res.status(500).send("Error deleting ticket")
+					res.status(500).send("Error updating ticket")
 					return
 				}
-				if (result.deletedCount === 0) {
+				if (result.modifiedCount === 0) {
 					res.status(404).send("Ticket not found")
 					return
 				}
-				res.sendStatus(204)
-			})
-	})
+				db
+					.collection("tickets")
+					.findOne({ _id: new mongodb.ObjectId(id) }, function (err, doc) {
+						if (err) {
+							console.error(err)
+							res.status(500).send("Error retrieving updated ticket")
+							return
+						}
+						res.send(doc)
+					})
+			}
+		)
+})
 
-	app.put("/rest/ticket/:id", function (req, res) {
-		const id = req.params.id
-		const updates = req.body
-		db
-			.collection("tickets")
-			.updateOne(
-				{ _id: new mongodb.ObjectId(id) },
-				{ $set: updates },
-				function (err, result) {
-					if (err) {
-						console.error(err)
-						res.status(500).send("Error updating ticket")
-						return
-					}
-					if (result.modifiedCount === 0) {
-						res.status(404).send("Ticket not found")
-						return
-					}
-					db
-						.collection("tickets")
-						.findOne({ _id: new mongodb.ObjectId(id) }, function (err, doc) {
-							if (err) {
-								console.error(err)
-								res.status(500).send("Error retrieving updated ticket")
-								return
-							}
-							res.send(doc)
-						})
-				}
-			)
-	})
+app.get("/newticket", function (req, res) {
+	res.sendFile(__dirname + "/newticket.html")
+})
+
+const port = 3000
+app.listen(port, function () {
+	console.log(`Server started on port ${port}`)
 })
